a4fcf8365bcd04b1c1e3135b3ff7967bd41ccbad
Adam <acdoyle630@gmail.com>
2017-09-26 10:41:20 -0700

    change word with button

--- nativeTesting/App.js
+++ nativeTesting/App.js
@@ -1,13 +1,38 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default class App extends React.Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      word : "app"
+    };
+  }
+
+
+  changeWord=()=>{
+    if(this.state.word === 'app'){
+      this.setState({
+        word : "Shit"
+      });
+    }
+    else{
+      this.setState({
+        word : "app"
+      })
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Some sort of {this.state.word}</Text>
+        <Button
+          onPress={this.changeWord}
+          title = 'Click me'
+        />
       </View>
     );
   }
